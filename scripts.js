771407d1dfd93f97b2f00052b621934cae942fd7
Danylo Bykon <93720610+dabyko@users.noreply.github.com>
2024-02-08 15:50:53 +0100

    create option to add tasks

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 let form = document.getElementById("form");
 let inputField = document.getElementById("input");
 let msg = document.getElementById("message");
+let tasks = document.getElementById("tasks");
 
 form.addEventListener("submit",(e)=>{
     e.preventDefault();
@@ -16,6 +17,26 @@ let formValidation = () => {
     else{
         msg.innerHTML = "";
         console.log("success");
+        acceptData();
     }
+};
+
+let data = {};
+
+let acceptData = () =>{
+    data["text"] = input.value;
+    console.log(data);
+    createTask();
+};
+
+let createTask = () => {
+    tasks.innerHTML += 
+    `<div>
+        <p>${data.text}</p>
+            <span class="options">
+                <i class="fas fa-edit"></i>
+                <i class="fas fa-trash-alt"></i>
+            </span>
+    </div>`;
 };
 
